cbecab28e34421d619960c24d1626c8d1c5e0caf
Tamas <hegtam90@gmail.com>
2020-12-28 17:52:09 +0100

    commit on master

---
+++ navbar.js
@@ -0,0 +1,26 @@
+const navbar = document.getElementById("navbar");
+const ham = document.getElementById("ham");
+
+const hamUrl = 'url("https://ljc-dev.github.io/testing0/ham.svg")';
+const closeUrl = 'url("https://ljc-dev.github.io/testing0/ham-close.svg")';
+
+console.log(getComputedStyle(navbar).getPropertyValue("-webkit-transform"));
+
+function toggleHamburger() {
+    if (getComputedStyle(ham).getPropertyValue('background-image') === hamUrl) {
+        ham.style.backgroundImage = closeUrl;
+        navbar.style.transform = "translateY(0)";
+    }
+    else {
+        ham.style.backgroundImage = hamUrl;
+        navbar.style.transform = "translateY(-100%)";
+    }
+
+    // navbar.classList.toggle("showNav");
+    // ham.classList.toggle("showClose");
+};
+
+
+// ham.addEventListener('click', function (event) {
+//     alert('Element clicked through function!');
+// });

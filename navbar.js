b6bb6cd06b694c82ba965394502a68d1d45a3437
Tamas <hegtam90@gmail.com>
2021-01-02 19:37:55 +0100

    commit on main

--- navbar.js
+++ navbar.js
@@ -9,11 +9,13 @@ console.log(getComputedStyle(navbar).getPropertyValue("-webkit-transform"));
 function toggleHamburger() {
     if (getComputedStyle(ham).getPropertyValue('background-image') === hamUrl) {
         ham.style.backgroundImage = closeUrl;
+        ham.style.backgroundColor = "transparent";
         navbar.style.transform = "translateY(0%)";
     }
     else {
         ham.style.backgroundImage = hamUrl;
         navbar.style.transform = "translateY(-100%)";
+        ham.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
     }
 
     // navbar.classList.toggle("showNav");
